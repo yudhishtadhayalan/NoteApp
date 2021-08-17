cd99dca2de3f9e6220ddf9d7c5ca1de20985754f
yudhishtadhayalan <yudhishtadhayalan@gmail.com>
2021-08-17 23:39:04 +0530

    display image from URL

--- Notes/Controller/NoteDetail/NoteDetailVC.swift
+++ Notes/Controller/NoteDetail/NoteDetailVC.swift
@@ -6,15 +6,18 @@
 //
 
 import UIKit
+import SDWebImage
 
 class NoteDetailVC: UIViewController {
     
     @IBOutlet weak var tblOutlet: UITableView!
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var btnBack: UIButton!
+    @IBOutlet weak var layout_ImageHeight: NSLayoutConstraint!
     
     var valuePassSelectedItem: Int?
     var modelNoteModelElement: [NoteModelElement]!
+    var isHidden: Bool?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,17 +30,28 @@ class NoteDetailVC: UIViewController {
         
         tblOutlet.delegate = self
         tblOutlet.dataSource = self
+        
+        let item_ = modelNoteModelElement?[valuePassSelectedItem ?? 0]
+        
+        imgView.sd_setImage(with: URL(string: "\(item_?.image ?? "")"), placeholderImage: #imageLiteral(resourceName: "placeHolder"))
+        
+        if item_?.image == nil || item_?.image == "" {
+            layout_ImageHeight.constant = 0
+        } else {
+            layout_ImageHeight.constant = 150
+        }
+        
     }
     
     @IBAction func didTapBack(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
     
-    
 }
 
+
 extension NoteDetailVC: UITableViewDelegate, UITableViewDataSource {
-        
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
@@ -47,15 +61,20 @@ extension NoteDetailVC: UITableViewDelegate, UITableViewDataSource {
         let item_ = modelNoteModelElement?[valuePassSelectedItem ?? 0]
         
         if indexPath.row == 0 {
-                    
+            
             let headerCell = tableView.dequeueReusableCell(withIdentifier: "NoteDetailHeaderCell") as! NoteDetailHeaderCell
             headerCell.lblHeading.text = item_?.title
-            headerCell.lblDate.text = item_?.time
+            
+            let timeFromAPI = Int64(item_?.time ?? "") ?? 0
+            
+            let convert_Milli_To_Date = Date(milliseconds: timeFromAPI)
+            headerCell.lblDate.text = "\(convert_Milli_To_Date)"
+            
             return headerCell
             
         } else {
             
-            let contentCell = tableView.dequeueReusableCell(withIdentifier: "NoteDetailTableCell") as! NoteDetailTableCell//tableView.dequeueReusableHeaderFooterView(withIdentifier: "NoteDetailHeaderCell") as! NoteDetailHeaderCell
+            let contentCell = tableView.dequeueReusableCell(withIdentifier: "NoteDetailTableCell") as! NoteDetailTableCell
             contentCell.lblContent.text = item_?.body
             return contentCell
             
@@ -75,11 +94,22 @@ extension NoteDetailVC: UITableViewDelegate, UITableViewDataSource {
     
 }
 
+
 //MARK: - Tool Bar change Batter and Time color --------------------------
 extension NoteDetailVC {
-
     override var preferredStatusBarStyle: UIStatusBarStyle {
-            return .lightContent
+        return .lightContent
+    }
+}
+
+
+//MARK: - Date ----------------------
+extension Date {
+    var millisecondsSince1970:Int64 {
+        return Int64((self.timeIntervalSince1970 * 1000.0).rounded())
     }
     
+    init(milliseconds:Int64) {
+        self = Date(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
+    }
 }
